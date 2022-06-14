df923c9204034da41ecadd9fac7d208157f26b8e
Amelie <amelierosser1986@gmail.com>
2022-06-14 10:56:41 -0400

    Add preload gpu

--- packages/intern-webgl-components/src/rendering/render-target.ts
+++ packages/intern-webgl-components/src/rendering/render-target.ts
@@ -10,12 +10,12 @@ import { WebGLRenderTarget, LinearFilter, NearestFilter, RGBFormat, UnsignedByte
  * @return {WebGLRenderTarget}
  */
 export function createRenderTarget(width: number = 1024, height: number = 1024, options: Object = {}) {
-    const defaults = {
-        minFilter: LinearFilter,
-        magFilter: NearestFilter,
-        format: RGBFormat,
-        type: UnsignedByteType,
-        stencilBuffer: false
-    };
-    return new WebGLRenderTarget(width, height, Object.assign({}, defaults, options));
+  const defaults = {
+    minFilter: LinearFilter,
+    magFilter: NearestFilter,
+    format: RGBFormat,
+    type: UnsignedByteType,
+    stencilBuffer: false
+  };
+  return new WebGLRenderTarget(width, height, Object.assign({}, defaults, options));
 }
