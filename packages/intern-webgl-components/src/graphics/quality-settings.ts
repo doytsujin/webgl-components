96033ede2bc19f7ded194d90c61e59c9532d1631
Amelie <amelierosser1986@gmail.com>
2022-06-20 13:22:19 -0400

    Remove anti alias

--- packages/intern-webgl-components/src/graphics/quality-settings.ts
+++ packages/intern-webgl-components/src/graphics/quality-settings.ts
@@ -1,7 +1,6 @@
 import { Vector2, MathUtils } from 'three';
 
 export type GraphicsSettings = {
-  antialias: boolean;
   pixelRatio: number;
   resolution: Vector2;
 };
@@ -14,17 +13,14 @@ export class Quality {
 
 export default class QualitySettings {
   high: GraphicsSettings = {
-    antialias: true,
     pixelRatio: MathUtils.clamp(window.devicePixelRatio, 1, 2),
     resolution: new Vector2(1920, 1080)
   };
   medium: GraphicsSettings = {
-    antialias: true,
     pixelRatio: MathUtils.clamp(window.devicePixelRatio, 1, 2),
     resolution: new Vector2(1920, 1080)
   };
   normal: GraphicsSettings = {
-    antialias: true,
     pixelRatio: 1,
     resolution: new Vector2(1280, 720)
   };
