205b59a12ec16b32345f33ca64a1ec92aa3f200f
Amelie <amelierosser1986@gmail.com>
2022-07-08 16:31:27 -0400

    fix: add image bitmap load support

--- packages/webgl-components/src/loading/three-texture-loader.ts
+++ packages/webgl-components/src/loading/three-texture-loader.ts
@@ -1,6 +1,15 @@
-import { TextureLoader, Texture } from 'three';
+import detect from '@jam3/detect';
+import { TextureLoader, Texture, ImageBitmapLoader, CanvasTexture, MathUtils, LinearFilter } from 'three';
 import Loader, { LoaderSettings } from './loader';
 import LoaderManager from './loader-manager';
+
+function validateTextureSize(width: number, height: number, texture: Texture) {
+  if (!(MathUtils.isPowerOfTwo(width) && MathUtils.isPowerOfTwo(height))) {
+    texture.generateMipmaps = false;
+    texture.minFilter = LinearFilter;
+    texture.magFilter = LinearFilter;
+  }
+}
 
 /**
  * Threejs texture loader
@@ -10,6 +19,16 @@ import LoaderManager from './loader-manager';
  * @extends {Loader}
  */
 export default class ThreeTextureLoader extends Loader {
+  // Same detection as
+  // https://github.com/mrdoob/three.js/blob/master/examples/jsm/loaders/GLTFLoader.js#L2407
+  bitmapSupported() {
+    return !(
+      typeof createImageBitmap === 'undefined' ||
+      detect.browser.safari ||
+      (detect.browser.firefox && detect.browser.majorVersion < 98)
+    );
+  }
+
   load = (settings?: LoaderSettings, manager: LoaderManager = new LoaderManager('three-texture-loader')) => {
     if (settings) {
       this.settings = Object.assign(this.settings, settings);
@@ -17,13 +36,34 @@ export default class ThreeTextureLoader extends Loader {
 
     manager.add(this);
 
-    const loader = new TextureLoader();
+    // Threejs:
+    // Use an ImageBitmapLoader if imageBitmaps are supported. Moves much of the
+    // expensive work of uploading a texture to the GPU off the main thread.
 
-    const onLoaded = (texture: Texture) => {
-      this.asset.data = texture;
-      this.emit('loaded', this.asset);
-    };
+    if (this.bitmapSupported()) {
+      const loader = new ImageBitmapLoader();
 
-    loader.load(this.asset.src, onLoaded, this.onProgress, this.onError);
+      loader.setOptions({
+        imageOrientation: 'flipY'
+      });
+
+      const onLoaded = (data: ImageBitmap) => {
+        const texture = new CanvasTexture(data);
+        validateTextureSize(texture.image.width, texture.image.height, texture);
+        this.asset.data = texture;
+        this.emit('loaded', this.asset);
+      };
+
+      loader.load(this.asset.src, onLoaded, this.onProgress, this.onError);
+    } else {
+      const loader = new TextureLoader();
+
+      const onLoaded = (texture: Texture) => {
+        this.asset.data = texture;
+        this.emit('loaded', this.asset);
+      };
+
+      loader.load(this.asset.src, onLoaded, this.onProgress, this.onError);
+    }
   };
 }
